db9e2b68f8274373234386ef54f80e197888087d
EI1024 <emi-iwado328@g.ecc.u-tokyo.ac.jp>
2023-04-06 15:26:25 +0900

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -5,27 +5,23 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-BLGRS7WXF0"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag() {dataLayer.push(arguments); };
+              gtag('js', new Date());
+              gtag('config', 'G-BLGRS7WXF0');
+            `
+          }}
+        />
         <title>Schole（スコレ）| 本格的プログラミング学習</title>
         <link rel="icon" href="/favicon.ico" />
         <style>
           @import
           url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&display=swap');
         </style>
-        {/* <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-LGX89LF33R"
-        ></script>
-
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag() { dataLayer.push(arguments); };
-          gtag('js', new Date());
-          gtag('config', 'G-LGX89LF33R');
-            `,
-          }}
-        /> */}
       </Head>
       <Component {...pageProps} />
     </>
