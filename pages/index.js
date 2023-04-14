3e3ccc0a4adda1a480ca318c8504b5af5d04ce97
EI1024 <emi-iwado328@g.ecc.u-tokyo.ac.jp>
2023-04-14 20:09:00 +0900

    update 404 page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 export default function Home() {
   return (
     <div>
-      <main>
-        <div className="text-center my-12 text-stone-600">404 | 存在しないページです</div>
+      <main className="text-center text-stone-600">
+        <div className="mt-12 mb-6">404 | 存在しないページです</div>
+        <div>Scholeのトップページは
+          <a href="https://campaign.schole.app/python-online"
+            className="underline text-cyan-700">こちら</a>
+        </div>
       </main>
     </div>
   )
