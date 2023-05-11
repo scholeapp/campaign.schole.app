ba76db9b97a2a535f70a2ee68c38fd6a76bbc457
N.Watanabe <asterisk37n@gmail.com>
2023-05-11 20:37:12 +0900

    Add Google Tag Manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import "../styles/globals.css";
+import Script from 'next/script'
 import Head from "next/head";
+
+const GTM_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_CONTAINER_ID
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+    // gtag.js (deprecated. Trying to migrate gtag.js into Google TagManager)
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-BLGRS7WXF0"></script>
         <script
           dangerouslySetInnerHTML={{
@@ -23,6 +27,19 @@ function MyApp({ Component, pageProps }) {
           url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&display=swap');
         </style>
       </Head>
+      <Script
+        id="gtag-base"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${GTM_ID}');
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </>
   );
