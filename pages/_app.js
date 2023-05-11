e1ce1c1655206fd10a8ceb7d7b46ef44d8030fef
N.Watanabe <asterisk37n@gmail.com>
2023-05-11 20:48:45 +0900

    fix jsx bug

--- pages/_app.js
+++ pages/_app.js
@@ -41,7 +41,7 @@ function MyApp({ Component, pageProps }) {
         }}
       />
       <Component {...pageProps} />
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=" + GTM_ID
+      <noscript><iframe src={"https://www.googletagmanager.com/ns.html?id=" + GTM_ID}
       height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     </>
   );
