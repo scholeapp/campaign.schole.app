732cd0997a6a741f552ba407ffeef0634ba1ca38
N.Watanabe <asterisk37n@gmail.com>
2023-05-12 00:42:07 +0900

    remove old gtag.js

--- pages/_app.js
+++ pages/_app.js
@@ -8,17 +8,6 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-BLGRS7WXF0"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag() {dataLayer.push(arguments); };
-              gtag('js', new Date());
-              gtag('config', 'G-BLGRS7WXF0');
-            `
-          }}
-        />
         <title>Schole（スコレ）| 本格的プログラミング学習</title>
         <link rel="icon" href="/favicon.ico" />
         <style>
